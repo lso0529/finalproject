eb88c50ccdc1e4ce68d06a4d6780f8b599cffd1b
JoygyuD <jgd940207@gmail.com>
2022-11-11 16:55:29 +0900

    member test

--- finalproject/src/main/java/org/zerock/controller/MemberController.java
+++ finalproject/src/main/java/org/zerock/controller/MemberController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/member/")
 public class MemberController {
+	
 	@RequestMapping("/mypage")
 	public String mypage() {
 		return "/member/mypage";
+	}
+	
+	@RequestMapping("/login")
+	public String loginPage() {
+		return"member/login";
 	}
 }
